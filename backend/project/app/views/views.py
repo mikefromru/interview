b0969937327ad932744879f0140dac6b2ec63696
mikefromru <googlockgood@gmail.com>
2024-01-07 00:46:37 +0300

    Added ListView for superuser

--- backend/project/app/views/views.py
+++ backend/project/app/views/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
 
-# Create your views here.
+
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from app.models import Subject
+
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+
+class SuperuserRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_superuser
+
+
+class SubjectListView(SuperuserRequiredMixin, ListView):
+    model = Subject
+
+
+class SubjectDetailView(SuperuserRequiredMixin, DetailView):
+    model = Subject
+
+
+class SubjectDeleteView(SuperuserRequiredMixin, DeleteView):
+    model = Subject
+
+
+class SubjectUpdateView(SuperuserRequiredMixin, UpdateView):
+    model = Subject
+
+
+class SubjectCreateView(SuperuserRequiredMixin, CreateView):
+    model = Subject
+
--- backend/project/project/urls.py
+++ backend/project/project/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from app.routers.api import api_urlpatterns
+# from app.routers.urls import urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
@@ -8,6 +9,7 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/app/', include(api_urlpatterns)),
+    path('myadmin/', include('app.routers.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 if settings.DEBUG:
